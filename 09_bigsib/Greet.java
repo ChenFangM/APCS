a11fea49d3d23d9b942405f288affccfd519df99
ChenFangM <Fang1113848389@gmail.com>
2021-10-03 14:20:14 -0400

    09 HW

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -17,9 +17,9 @@ public class Greet {
 
 /*
 DISCOVERIES
-- In order to use the methods in class BigSib, we need to have a way to refer to another file (i.e. via packages).
-- Before we can import BigSib, we can create a package using the "package" command in BigSib.java.
+- In order to use the methods in class BigSib, we must create BigSib.java so that BigSib.greet refers to a the BigSib class.
+- Importing a package is not necessary to use a method from the same directory.
 
 UNSOLVED QUESTIONS
-- Is there another way in which we cna use methods/classes from another file in the same directory without creating a package?
+- Is there another way in which we can use methods/classes from another file in the same directory without creating?
 */
