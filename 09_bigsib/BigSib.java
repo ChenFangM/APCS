d3dfc4d21bc157eb9af0fc8c98e2ca20ac2971d4
ChenFangM <Fang1113848389@gmail.com>
2021-10-04 21:11:40 -0400

    09 HW

--- 09_bigsib/BigSib.java
+++ 09_bigsib/BigSib.java
@@ -1,3 +1,10 @@
+/*
+Team Not-In-Service {Fang Chen, nada Hameed, Prattay Dey, and Ducky Cat}
+APCS
+HW 09 -- Using BigSib class in Greet class
+2021-10-01
+*/
+
 public class BigSib {
 
 	public static void main(String[] args) {
@@ -11,3 +18,11 @@ public class BigSib {
 
 }
 
+/*
+DISCOVERIES
+- In order to use the methods in class BigSib, we must create BigSib.java with a greet method so that BigSib.greet will know where to refer to.
+- Importing a package is not necessary to use a method from a file in the same directory.
+
+UNSOLVED QUESTIONS
+- Is there another way in which we can use methods/classes from another file in the same directory or even different directories?
+*/
--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,10 +1,3 @@
-/*
-Team Not-In-Service {Fang Chen, Nada Hameed, Prattay Dey, and Cat}
-APCS
-HW09 -- Using BigSib class in Greet class
-2021-10-01
-*/
-
 public class Greet {
 
     public static void main(String [] args) {
@@ -14,12 +7,3 @@ public class Greet {
     }
     
 }
-
-/*
-DISCOVERIES
-- In order to use the methods in class BigSib, we must create BigSib.java so that BigSib.greet refers to a the BigSib class.
-- Importing a package is not necessary to use a method from the same directory.
-
-UNSOLVED QUESTIONS
-- Is there another way in which we can use methods/classes from another file in the same directory without creating?
-*/
