1efbb211323762f4ff76ca775e63499b6ad7a741
ChenFangM <Fang1113848389@gmail.com>
2021-10-04 21:49:26 -0400

    10 HW

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,12 @@
+public class BigSib {
+
+	public static void main(String[] args) {
+		System.out.println( greet("Testing"));
+	}
+
+	public static String greet(String name) {
+		String greeting = "Good morning! How has your day been, " + name + "?";
+		return greeting;
+	}
+
+}
