4824d13e88d51291c6b051a57444ec070c99247d
ChenFangM <Fang1113848389@gmail.com>
2021-10-05 21:55:55 -0400

    12 HW

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,30 @@
+/* 
+Team Dynastic Duo of Destiny {Fang, Ducky Cat, William, and Ducky CS5ODDB}
+APCS
+HW 12 -- On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-26
+*/
+
+public class BigSib {
+
+	String helloMsg;
+
+	public void setHelloMsg(String msg) {
+		helloMsg = msg;
+	}
+
+	public String greet(String name) {
+		String greeting = helloMsg + " " + name + "\n" + "Salutations Dr.Spaceman" + "\n" + "Hey ya Kong Fooey" + "\n" + "Sup mom";
+		return greeting;
+	}
+
+}
+
+/*
+DISCOVERIES
+- Too much
+
+QCC
+- Everything you didn't teach
+
+*/
