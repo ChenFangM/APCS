67a436e59f72e11da0a2dd8fb5e00c8fad00c31d
ChenFangM <Fang1113848389@gmail.com>
2021-10-06 22:15:04 -0400

    13 HW

--- 13_hatch/Greet.java
+++ 13_hatch/Greet.java
@@ -7,28 +7,24 @@ public class Greet {
 		//
 
 		BigSib richard = new BigSib("Word up");
-		
 		greeting = richard.greet("freshman");
 		System.out.println(greeting);
 
 		//
 
 		BigSib doctor = new BigSib("Salutations");
-
 		greeting = doctor.greet("Dr. Spaceman");
 		System.out.println(greeting);
 
 		//
 
 		BigSib fooey = new BigSib("Hey ya");
-
 		greeting = fooey.greet("Kong Fooey");
 		System.out.println(greeting);
 
 		//
 
 		BigSib mom = new BigSib("Sup");
-
 		greeting = mom.greet("mom");
 		System.out.println(greeting);
 
