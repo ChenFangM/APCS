ba62ac082d84bbd81cd34daf3d1a68c10259a976
ChenFangM <Fang1113848389@gmail.com>
2022-02-05 22:20:49 -0500

    lab 05

--- lab05_statprinter/StatPrinter.java
+++ lab05_statprinter/StatPrinter.java
@@ -1,56 +1,10 @@
 // Butterfingers: Melody Lew, Corina Chen, Fang Min Chen
 // APCS pd6
 // L05 -- pulling it together
-// 2022-02-03r
+// 2022-02-07m
 // time spent: 2.0 hrs
 
-
-/**
-   An AP-style question, for practice...
-
-   GOAL: Write the StatPrinter class below.
-
-   WHAT YOU NEED TO KNOW:
-   The StatPrinter Object receives an
-   ArrayList of nonnegative integers, then builds a frequency ArrayList in which
-   the index values are the data and the entry at the index is the frequency.
-   For example, if the received data is    2,3,2,5,1,3    then the frequency
-   list would be [0,1,2,2,0,1]. This is read as 0 zeroes, 1 one, 2 twos,
-   2 threes, 0 fours, 1 five. The size of the frequency list is the equal to
-   the maximum value of the data.
-
-   A capability of the class is to calculate local modes from the frequency
-   list. A local mode is a value that is greater than the value at index-1
-   and greater than the value at index+1. A local mode is never at the end
-   points of the list. For example, if the frequency list is [1,2,1,4,2,3,5]
-   then the local modes are 2 and 4.
-
-   This class is also capable of printing a histogram of the frequencies, using
-   '*'s to indicate a frequency amount. To print a histogram, the user specifies
-   the longest sequence of '*'s used and then all other values are printed in
-   proportion to this value. For example, if longest bar is 10 and the frequency
-   list is [1,2,1,4,2,3,5] then the histogram printed looks like this:
-
-   0 : **
-   1 : ****
-   2 : **
-   3 : ********
-   4 : ****
-   5 : ******
-   6 : **********
-
-   For each method, state run time efficiency using Big O notation.
-
-   TIPS FOR AWESOME:
-   * Keys to Success were so named (by Thinkeren) for a reason.
-   * Look over all fxns, think a bit, decide which to tackle first.
-   ( Simplest?  Prerequisites? . . . )
-   * Develop 1 fxn at a time, test it, then move to next.
-   * Initially, what extra code do you need to get past compiler?
-   **/
-
 import java.util.ArrayList;
-
 
 public class StatPrinter
 {
@@ -73,7 +27,7 @@ public class StatPrinter
     for (Integer j : data){
       _frequency.set(j,_frequency.get(j)+1);
     }
-  }
+  } //big O notation: O(n)
 
 
   //*************** QUESTION 01 **************************
@@ -88,7 +42,7 @@ public class StatPrinter
       }
     }
     return currentMax;
-  }
+  } //big O notation: O(n)
 
 
   //*************** QUESTION 03 **************************
@@ -103,8 +57,10 @@ public class StatPrinter
   //    isLocalMode(5) -> true
   public boolean isLocalMode( int i )
   {
-    return (i > 0 && i < _frequency.size() - 1 && _frequency.get( i - 1 ) < _frequency.get( i ) && _frequency.get( i + 1 ) < _frequency.get( i ));
-  }
+    return (i > 0 && i < _frequency.size() - 1
+    && _frequency.get( i - 1 ) < _frequency.get( i )
+    && _frequency.get( i + 1 ) < _frequency.get( i ));
+  } //big O notation: O(1)
 
 
   //*************** QUESTION 04 **************************
@@ -118,7 +74,7 @@ public class StatPrinter
       }
     } return mode;
 
-  }
+  } //big O notation: O(n)
 
 
   //*************** QUESTION 05 **************************
@@ -132,6 +88,6 @@ public class StatPrinter
         System.out.print("*");
       }
     } System.out.println();
-  }
+  } //big O notation: O(n^2)
 
 }//end class StatPrinter
