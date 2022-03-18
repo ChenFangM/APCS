da7e55fb8914881beabc3d78bfeeef71f7519553
ChenFangM <Fang1113848389@gmail.com>
2022-03-18 12:19:55 -0400

    act 1

--- cobo/elevens/Activity1 Starter Code/CardTester.java
+++ cobo/elevens/Activity1 Starter Code/CardTester.java
@@ -8,6 +8,15 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card A = new Card("2", "Spade", 2);
+		Card B = new Card("A", "Diamond", 1);
+		Card C = new Card("A", "Diamond", 1);
+
+		System.out.println(A);
+		System.out.println(B);
+		System.out.println(C);
+
+		System.out.println(A.matches(B));
+		System.out.println(B.matches(C));
 	}
 }
