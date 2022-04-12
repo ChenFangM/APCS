f629d07acba4a04b9a50f82fb6210cde5b653e19
Fang Chen <90415179+ChenFangM@users.noreply.github.com>
2022-04-12 02:40:22 -0400

    89 HW

--- 89_queue/LLQueue.java
+++ 89_queue/LLQueue.java
@@ -11,7 +11,7 @@ public class LLQueue<P> implements Queue<P>
 
     private LinkedList<P> _queue;
 
-    public ALQueue {
+    public LLQueue() {
         _queue = new LinkedList<P>();
     }
 
@@ -20,45 +20,46 @@ public class LLQueue<P> implements Queue<P>
     public P dequeue() 
     {
         return _queue.removeFirst();
-    }
+    } // O(1)
 
     //means of adding an element to collection:
     //Enqueue an element onto the back of this queue.
     public void enqueue( P x ) {
         _queue.add(x);
-    }
+    } // O(1)
 
     //Returns true if this queue is empty, otherwise returns false.
     public boolean isEmpty() {
         return _queue.isEmpty();
-    }
+    } // O(1)
 
     //Returns the first element of the queue without dequeuing it.
     public P peekFront() {
         return _queue.peekFirst();
-    }
+    } // O(1)
 
     //toString
     public String toString() {
         String str = "";
+        if (_queue.isEmpty()) { return str; }
         for (P item : _queue) {
-            str += (String) item;
+            str += (String) item + ", ";
         }
-        return str;
+        return str.substring(0, str.length() - 2);
     }
 
     //main method for testing
     public static void main(String[] args) 
     {
-        LLQueue<String> test = new LLQueue<String>;
+        LLQueue<String> test = new LLQueue<String>();
 
-        System.println("Empty? " + test.isEmpty());
+        System.out.println("Empty? " + test.isEmpty());
         System.out.println(test); 
 
         test.enqueue("Fang");
         test.enqueue("Sophia");
         test.enqueue("Ameer");
-        System.println("Empty? " + test.isEmpty());
+        System.out.println("Empty? " + test.isEmpty());
         System.out.println(test);
 
         System.out.println(test.peekFront());
