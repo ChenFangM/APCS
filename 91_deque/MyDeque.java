99c6c1a31244249b803e9b063a755d6dd524b3be
Fang Chen <90415179+ChenFangM@users.noreply.github.com>
2022-04-13 12:30:37 -0400

    91 HW

--- 91_deque/MyDeque.java
+++ 91_deque/MyDeque.java
@@ -65,30 +65,4 @@ public class MyDeque<Card> implements Deque<Card>
         return _deque.size();
     }
 
-    public static void main(String[] args) {
-      MyDeque<String> test = new MyDeque<String>();
-
-        System.out.println("Empty? " + test.isEmpty());
-
-        test.addFirst("Fang");
-        test.addFirst("Sophia");
-        test.addLast("Ameer");
-        System.out.println("Empty? " + test.isEmpty());
-
-        System.out.println(test.peekFirst());
-        System.out.println(test.peekLast());
-
-        test.removeFirst();
-        System.out.println(test.peekFirst());
-        System.out.println(test.peekLast());
-
-
-        test.removeLast();
-        System.out.println(test.peekFirst());
-        System.out.println(test.peekLast());
-
-        test.removeLast();
-        System.out.println(test.peekFirst());
-        System.out.println(test.peekLast());
-    }
 }
